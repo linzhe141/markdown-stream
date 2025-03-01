6fadd615fd39385217b9410b967e476ba5eb6cc0
linzhe <linzhe141@qq.com>
2025-03-01 16:51:37 +0800

    feat: add loading state to re-render button in App.vue

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import { md } from "./md";
 import { createMarkdownStreamRender } from "../../src";
 const container = ref<Element | null>(null);
+const disabled = ref(false);
 function createStream(text: string, chunkSize = 10, delay = 50) {
   let position = 0;
 
@@ -27,18 +28,39 @@ function createStream(text: string, chunkSize = 10, delay = 50) {
     },
   });
 }
-function clickHandle() {
+async function clickHandle() {
+  if (disabled.value) return;
+  disabled.value = true;
   const stream = createStream(md);
   const containerDom = container.value!;
-  createMarkdownStreamRender(stream, containerDom);
+  containerDom.innerHTML = "";
+  await createMarkdownStreamRender(stream, containerDom);
+  disabled.value = false;
 }
 </script>
 <template>
   <div class="w-screen h-screen py-16 mx-auto max-w-[800px]">
     <button
-      class="bg-[#0a0a0a] text-white py-2 px-4 rounded-sm border border-[#262626] hover:bg-[#262626] cursor-pointer"
+      class="flex bg-[#0a0a0a] text-white py-3 px-4 rounded-sm border border-[#262626] hover:bg-[#262626]"
+      :class="{
+        'cursor-not-allowed': disabled,
+        'cursor-pointer': !disabled,
+      }"
       @click="clickHandle"
     >
+      <div v-if="disabled" class="animate-spin size-6 mr-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24px"
+          height="24px"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M12 4V2A10 10 0 0 0 2 12h2a8 8 0 0 1 8-8"
+          ></path>
+        </svg>
+      </div>
       Re-render
     </button>
     <div
